ae125ecc140f3e96740503b5d57d1fdf5a104769
trezhang <trezhang@tencent.com>
2023-09-23 10:58:41 +0800

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zhangzerui20/puppy
+
+go 1.20
---
+++ puppy.go
@@ -0,0 +1,11 @@
+package puppy
+
+import "fmt"
+
+func bark() {
+	fmt.Println("Woof!")
+}
+
+func barks() {
+	fmt.Println("Woof! Woof! Woof!")
+}
