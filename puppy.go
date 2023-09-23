dd69b052eb479e5734f57cc9ed42ee276d81c7c4
trezhang <trezhang@tencent.com>
2023-09-23 18:38:30 +0800

    second

--- puppy.go
+++ puppy.go
@@ -3,9 +3,17 @@ package puppy
 import "fmt"
 
 func Bark() {
-	fmt.Println("Woof!")
+	fmt.Println(GetBark())
+}
+
+func GetBark() string {
+	return "Woof!"
 }
 
 func Barks() {
-	fmt.Println("Woof! Woof! Woof!")
+	fmt.Println(GetBarks())
+}
+
+func GetBarks() string {
+	return "Woof! Woof! Woof!"
 }
